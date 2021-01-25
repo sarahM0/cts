2d4739fe2a0e088f315d0f03f399d488c313e7f4
sarahM0 <9856269+sarahM0@users.noreply.github.com>
2021-01-25 21:13:19 +0000

    Deploying to gh-pages from  @ 97add94a58cbe0233f5dd619430d07e40f465b2c 🚀

---
+++ out/common/framework/version.js
@@ -0,0 +1,3 @@
+// AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
+
+export const version = '97add94a58cbe0233f5dd619430d07e40f465b2c';
--- out/tools/glsl.macro.js
+++
@@ -1,9 +0,0 @@
-/**
-* AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts
-**/
-
-import { unreachable } from '../framework/util/index.js';
-export default function GLSL(stage, source) {
-  unreachable('macro stub implementation');
-}
-//# sourceMappingURL=glsl.macro.js.map
